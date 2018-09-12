31728e610d03eec832c2df5f6b3d1923180729f9
Worrapong Saewong <chan.worrapong@rabbitstale.com>
2018-09-12 15:15:47 +0700

    update

--- frontend/global.js
+++ frontend/global.js
@@ -1,18 +1,51 @@
 $(function() {
 
-	alert(todo_name);
+getItemList();
 
-	function getList(){
-		$.ajax({
-			url : '',
-		           method : "GET",
-		           success : function (data){
+	function getItemList(){
+		var html = ""
+		           		html += '<div class="listItem p-3">'
+						html +=		'<div class="form">'
+						html +=		'<div class="form-group form-row row">'
+						html +=					'<div class="col-10">'
+						html +=				      	'<label>'
+						html +=				      		'<input type="checkbox" class="mr-3"> just a test items'
+						html +=				      	'</label>'
+						html +=					'</div>'
+						html +=					'<div class="col-2">'
+						html +=						'<button class="btn btn-sm">Delete</button>'
+						html +=					'</div>'
+						html +=				'</div>'
+						html +=			'</div>'
+						html +=		'</div>'
+								
+						$('#todoList').append(html);
+		// $.ajax({
+		// 	url : 'http://workshopapi.democnc.com/api/todo',
+		//            method : "GET",
+		//            success : function (data){
+		//            		var html = ""
+		//            		html = '<div class="listItem p-3">\
+		// 							<div class="form">\
+		// 								<div class="form-group form-row row">\
+		// 									<div class="col-10">\
+		// 								      	<label>\
+		// 								      		<input type="checkbox" class="mr-3"> just a test items\
+		// 								      	</label>\
+		// 									</div>\
+		// 									<div class="col-2">\
+		// 										<button class="btn btn-sm">Delete</button>\
+		// 									</div>			\		
+		// 								</div>\
+		// 							</div>\
+		// 						</div>';
 
-		           },
-		           error : function (data) {
+		// 				$('#todoList').append(html);
+		//            },
+		//            error : function (data) {
 
-		           },
-		})
+		//            },
+		// })
 	}
 
 	function editItem(){
