96f8d76b1df5e0e67008020e046f6996abd8d72b
Ton <ton.tosapon@rabbitstale.com>
2018-09-12 13:58:18 +0700

    Create API Route and TodosController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('todos', [
+    'as'   => 'api.todos.index',
+    'uses' => 'Api\TodosController@index'
+]);
