98989bd04ff89d22e961278a3d1fd2478006ac17
Worrapong Saewong <chan.worrapong@rabbitstale.com>
2018-09-12 14:54:50 +0700

    set method

--- frontend/global.js
+++ frontend/global.js
@@ -1,1 +1,57 @@
-alert('hello');
+
+$(function() {
+
+	alert(todo_name);
+
+	function getList(){
+		$.ajax({
+			url : '',
+		           method : "GET",
+		           success : function (data){
+
+		           },
+		           error : function (data) {
+
+		           },
+		})
+	}
+
+	function editItem(){
+		$.ajax({
+			url : '',
+		           method : "POST",
+		           success : function (data){
+
+		           },
+		           error : function (data) {
+
+		           },
+		})
+	}
+
+	function deleteItem(){
+		$.ajax({
+			url : '',
+		           method : "DELETE",
+		           success : function (data){
+
+		           },
+		           error : function (data) {
+
+		           },
+		})
+	}
+
+	function updateStatus(){
+		$.ajax({
+			url : '',
+		           method : "POST",
+		           success : function (data){
+
+		           },
+		           error : function (data) {
+
+		           },
+		})
+	}
+});  
